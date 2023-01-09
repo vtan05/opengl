e199043a5b0b1a77b0fd1d403553b53d0541278d
DESKTOP-2SSVG8K\vanta <vantan2555@gmail.com>
2023-01-09 13:15:05 +0900

    Load texture

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -14,7 +14,7 @@ class Shader
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
 
 public:
 	Shader(const std::string& filepath);
@@ -24,11 +24,13 @@ public:
 	void UnBind() const;
 
 	// Set uniforms
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
-	unsigned int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name);
 };
